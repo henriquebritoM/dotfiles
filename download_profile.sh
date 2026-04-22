8e0c08f46e8b890a0add38426f723c2ad1a9203c
henriquebritoM <henrique.britomelo667@gmail.com>
2026-04-22 18:14:14 -0300

    Reestructure: Initial Commit

--- download_profile.sh
+++
@@ -1,8 +0,0 @@
-sudo bash setup.sh
-
-echo "reloading do terminal"
-source ~/.bashrc
-
-echo "instalando perfil"
-konsave -i "./konsave_profile"
-konsave -a "hb"
--- upload_config.sh
+++
@@ -1,8 +0,0 @@
-sudo bash setup.sh
-
-echo "reloading do terminal"
-source ~/.bashrc
-
-echo "salvando profile konsave"
-konsave -s "hb"
-konsave -e "hb" -d konsave_profile -n "hb" -f
